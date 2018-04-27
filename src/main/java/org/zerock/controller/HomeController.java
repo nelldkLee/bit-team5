e0083bb07f1331e059570557f772a6f912f51a5a
wjdrn <wjdrn@DESKTOP-K043QL5>
2018-04-27 16:08:08 +0900

    Commit test

--- src/main/java/org/zerock/controller/HomeController.java
+++ src/main/java/org/zerock/controller/HomeController.java
@@ -31,7 +31,7 @@ public class HomeController {
 		
 		String formattedDate = dateFormat.format(date);
 		
-		model.addAttribute("serverTime HI", formattedDate );
+		model.addAttribute("serverTime hi", formattedDate );
 		
 		return "home";
 	}
